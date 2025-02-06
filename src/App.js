3e7e8f7bf9ef790b6496fd89935507fc6a87188a
Your Name <your-email@example.com>
2025-02-06 22:14:18 +0530

    Class Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-function App(){
-  return(
-    <div className="App">
-      <h1>Functional Component</h1>
-    </div>
-  )
+import React from "react";
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className="App">
+        <h1>This is Class Component</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
