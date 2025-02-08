e2b0216df83e8a527ce4b89e07f81626317b5e58
Your Name <your-email@example.com>
2025-02-08 21:56:34 +0530

    function call from onClick

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App(){
   
   return(
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello World !</h1>
       <button onClick={apple}>Click me</button>
       </div>
   );
